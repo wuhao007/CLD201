3b5d1f93c78eb54d99128b778bee41eb2444befd
Hao Wu <tohaowu@gmail.com>
2015-05-23 14:56:14 -0700

    I almost forgot this code

--- OutlookAppDemo/OutlookAppDemoWeb/AppRead/Home/Home.js
+++ OutlookAppDemo/OutlookAppDemoWeb/AppRead/Home/Home.js
@@ -15,20 +15,9 @@
     // Displays the "Subject" and "From" fields, based on the current mail item
     function displayItemDetails() {
         var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
-        $('#subject').text(item.subject);
+        var meetingSuggestions = item.getEntitiesByType(Office.MailboxEnums.EntityType.MeetingSuggestion);
+        var meetingSuggestion = meetingSuggestions[0];
 
-        var from;
-        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
-            from = Office.cast.item.toMessageRead(item).from;
-        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
-            from = Office.cast.item.toAppointmentRead(item).organizer;
-        }
-
-        if (from) {
-            $('#from').text(from.displayName);
-            $('#from').click(function () {
-                app.showNotification(from.displayName, from.emailAddress);
-            });
-        }
+        $('#start-time').text(meetingSuggestion.start);
     }
 })();
---
+++ WordDemo/WordDemoWeb/App/Home/Home.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#create-link').click(getDataFromSelection);
+        });
+    };
+
+    // Reads data from current document selection and displays a notification
+    function getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    var text = result.value;
+                    var hyperlink = '<a href="http://www.bing.com/search?q=' + text + '">' + text + '</a>';
+                    setDataSelection(hyperlink);
+                } else {
+                    app.showNotification('Error:', result.error.message);
+                }
+            }
+        );
+    }
+
+    function setDataSelection(hyperText) {
+        Office.context.document.setSelectedDataAsync(hyperText, { coercionType: 'html' },
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('Success:', 'Link created!');
+                } else {
+                    app.showNotification('Error:', result.error.message);
+                }
+        });
+    }
+})();
